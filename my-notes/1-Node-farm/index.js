5e3f3316e41d747122ae74b5602ac02802416651
zubairch1199 <133282511+chshahid119@users.noreply.github.com>
2024-03-12 02:44:01 +0500

    create a simple api

---
+++ my-notes/1-Node-farm/index.js
@@ -0,0 +1,66 @@
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+
+/////////////////////////////////////////////
+// FILES HANDLING
+
+// Blocking, synchronus way
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+
+// console.log(textIn);
+
+// const textOut = `This is what we know about avocado: ${textIn}.\nCreate on ${new Date(
+//   Date.now()
+// ).toString()}`;
+
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("File is written!");
+
+// Non-blocking,asynchronus way
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   if (err) return console.log("Error! 🔥 ");
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
+//     fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+//       console.log(data3);
+
+//       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+//         console.log("Your filw has been written 😊");
+//       });
+//     });
+//   });
+// });
+
+// console.log("will read this file first");
+
+/////////////////////////////////////////////
+// SERVER
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const productData = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+  const pathName = req.url;
+
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the OVERVIEW");
+  } else if (pathName === "/product") {
+    res.end("This is the PRODUCT");
+  } else if (pathName === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    // res.end("API");
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("<h1>Page not found!</h1>");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to request on port 8000");
+});
