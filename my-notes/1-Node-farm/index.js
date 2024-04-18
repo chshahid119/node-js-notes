74352febdbeb6863c57e392c7db6cb3846fbd9c2
zubairch1199 <133282511+chshahid119@users.noreply.github.com>
2024-04-19 02:58:13 +0500

    module 2 again started for revise

--- my-notes/1-Node-farm/index.js
+++ my-notes/1-Node-farm/index.js
@@ -39,64 +39,13 @@ const replaceTemplate = require("./modules/replaceTemplate");
 // console.log("will read this file first");
 
 /////////////////////////////////////////////
-console.log(slugify("Fresh Avocados", { lower: true }));
-
-const tempOverview = fs.readFileSync(
-  `${__dirname}/templates/template-overview.html`,
-  "utf-8"
-);
-const tempCard = fs.readFileSync(
-  `${__dirname}/templates/template-card.html`,
-  "utf-8"
-);
-const tempProducts = fs.readFileSync(
-  `${__dirname}/templates/template-product.html`,
-  "utf-8"
-);
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
-const productData = JSON.parse(data);
-
-const slugs = productData.map((el) => slugify(el.productName, { lower: true }));
-console.log(slugs);
 
 // SERVER
 const server = http.createServer((req, res) => {
-  console.log(req.url);
-  const { query, pathname } = url.parse(req.url, true);
-
-  // OVERVIEW PAGE
-  if (pathname === "/" || pathname === "/overview") {
-    res.writeHead(200, { "Content-type": "text/html" });
-
-    const cardsHtml = productData
-      .map((el) => replaceTemplate(tempCard, el))
-      .join("");
-    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
-    // console.log(cardsHtml);
-    res.end(output);
-
-    // PRODUCT PAGE
-  } else if (pathname === "/product") {
-    res.writeHead(200, { "Content-type": "text/html" });
-    const product = productData[query.id];
-    const output = replaceTemplate(tempProducts, product);
-    res.end(output);
-
-    // API
-  } else if (pathname === "/api") {
-    res.writeHead(200, { "Content-type": "application/json" });
-    // res.end("API");
-    res.end(data);
-
-    // NOT FOUND
-  } else {
-    res.writeHead(404, {
-      "Content-type": "text/html",
-      "my-own-header": "hello-world",
-    });
-    res.end("<h1 >Page not found!</h1>");
-  }
+  console.log(req);
+  res.end("Hello from the Server!");
 });
+
 server.listen(8000, "127.0.0.1", () => {
   console.log("Listening to request on port 8000");
 });
