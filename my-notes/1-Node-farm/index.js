d3ceb3b37e63711decd69333da09115582cbdd47
zubairch1199 <133282511+chshahid119@users.noreply.github.com>
2024-04-19 03:45:47 +0500

    make a samall api

--- my-notes/1-Node-farm/index.js
+++ my-notes/1-Node-farm/index.js
@@ -41,9 +41,26 @@ const replaceTemplate = require("./modules/replaceTemplate");
 /////////////////////////////////////////////
 
 // SERVER
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
-  console.log(req);
-  res.end("Hello from the Server!");
+  const pathName = req.url;
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the OVERVIEW");
+  } else if (pathName === "/product") {
+    res.end("This is the PRODUCT");
+  } else if (pathName === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello world",
+    });
+    res.end("<h1>Page not Found!</h1>");
+  }
 });
 
 server.listen(8000, "127.0.0.1", () => {
