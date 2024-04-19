94ed87e34409e20b14f4485bfcc995516b46dd81
zubairch1199 <133282511+chshahid119@users.noreply.github.com>
2024-04-20 00:46:22 +0500

    some changes  made

--- my-notes/1-Node-farm/index.js
+++ my-notes/1-Node-farm/index.js
@@ -57,6 +57,9 @@ const tempProduct = fs.readFileSync(
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+console.log(slugs);
+
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
 
